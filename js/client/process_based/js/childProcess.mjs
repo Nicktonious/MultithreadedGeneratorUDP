d0f06b74bec2f4e29cce80ec56aeb5e9b7a5ea4b
Nicktonious <medovoy1213@gmail.com>
2025-08-27 16:02:41 +0400

    переименован файл захардкожен вызов метода RunBrokerSpeed, теперь являющегося основным при выборе ядра для привязки добавлен сдвиг на 1, обеспечивающий что процесс не будет делить ядро с main процессом

--- js/client/process_based/js/childProcess.mjs
+++ js/client/process_based/js/childProcess.mjs
@@ -1,12 +1,25 @@
 import os, { cpus } from 'os';
-import Sender from './Sender.mjs';
+import Sender from './ClassSender.mjs';
 import { argv } from 'process';
 import { execSync } from 'child_process';
 
 const [path, fn, args] = argv;
 let workerData = JSON.parse(args);
 
-const sender = new Sender(workerData);
+// Привязка к CPU-ядру через taskset (Linux)
+if (os.type() == 'Linux') {
+    const { pid } = process;
+    let i = workerData.threadIndex;
+    let { baseCPUIndex } = workerData;
+    const cpu = baseCPUIndex + ((i+1) % cpus().length);
+    execSync(`taskset -cp ${cpu} ${pid}`);
+    console.log(`Process ${process.pid} running on Core ${cpu}`);
+}
+
+const sender = new Sender(workerData, 5555);
+await sender.Init();
+sender.RunBrokerSpeed()
+
 process.on('message', (msg) => {
     console.log(msg);
     if (msg.type === 'SIGINT') {
@@ -14,18 +27,7 @@ process.on('message', (msg) => {
     }
 });
 
-// Привязка к CPU-ядру через taskset (Linux)
-if (os.type() == 'Linux') {
-    const { pid } = process;
-    let i = workerData.threadIndex;
-    let { baseCPUIndex } = workerData;
-    const cpu = baseCPUIndex + (i % cpus().length);
-    execSync(`taskset -cp ${cpu} ${pid}`);
-    console.log(`Process ${process.pid} running on Core ${cpu}`);
-}
-
-await sender.Init();
-if (workerData.isTriang)
-    sender.RunTriangleSpeed(workerData);
-else 
-    sender.RunFixedSpeed(workerData);
+// if (workerData.isTriang)
+//     sender.RunTriangleSpeed(workerData);
+// else 
+//     sender.RunFixedSpeed(workerData);
