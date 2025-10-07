150ddff23eeb5b8b49890807febcfd7ef7f7b812
Nicktonious <medovoy1213@gmail.com>
2025-10-07 17:17:11 +0400

    контролльное сохранение более не используемых файлов, в следующем пул реквесте будут удалены

--- js/client/process_based/js/ClassZMQServer.mjs
+++ js/client/process_based/js/ClassZMQServer.mjs
@@ -1,29 +1,37 @@
 import zmq from 'zeromq';
 import { performance } from 'perf_hooks';
 import fs from 'node:fs';
-import { sleep } from './utils.mjs';
+import { getIncrIPCAddress, sleep } from './utils.mjs';
+import crypto from 'crypto';
 
 class ClockGenerator {
-    constructor({ address }) {
+    constructor({ address, n, m }) {
         this.address = address;
+        // this.publisher = new zmq.Push();
+        console.log(n, m);
         this.publisher = new zmq.Publisher();
+
+        this.n = n;
+        this.m = m;
         this.stopFlag = false;
         this.messageCounter = 0;
     }
+
     async Init() {
-        await this.publisher.bind(this.address);
-        console.log(`ZMQ Publisher-брокер запущен на ${this.address}`);
+        /*const ipcDir = '/tmp/zmq_clock';
+        if (!fs.existsSync(ipcDir)) {
+            fs.mkdirSync(ipcDir, { recursive: true });
+        }*/
+        this.publisher.bind(this.address);
+        console.log(`ZMQ Clock broker is bound to: ${this.address}`);
+
+        /*const address = `ipc://${ipcDir}_${i}.ipc`;
+        // const address = getIncrIPCAddress(this.address, i);
+        await this.publisher.bind(address);
+
+        // console.log(`ZMQ Publisher-брокеры запущены на ${addrArr}`);*/
         await sleep(100);
         return this;
-    }
-
-    async Start(freq, limit, cb) {
-        // await this.publisher.bind(`tcp://*:${this.port}`);
-        await this.publisher.bind(this.address);
-        await sleep(100);
-        console.log(`ZMQ Publisher-брокер запущен на ${this.address}`);
-
-        await this.Run(freq, limit, cb);
     }
 
     /**
@@ -48,25 +56,62 @@ class ClockGenerator {
         }
     }
 
-    async Run(freq, limit=Number.MAX_SAFE_INTEGER, cb=()=>{}) {
-        const targetInterval = 1/freq *1000 * 1000; // Целевой интервал в микросекундах
+    /*async Run(freq, limit = Number.MAX_SAFE_INTEGER, cb = () => { }) {
+        const targetInterval = 1 / freq * 1000 * 1000; // Целевой интервал в микросекундах
         console.log(`Целевой интервал: ${targetInterval} мкс`);
         let t_0 = performance.now();
+        let t_1;
         try {
             for await (const _ of this.intervalGenerator(targetInterval)) {
-                if (this.stopFlag) break;
+                if (this.stopFlag) { t_1 = performance.now(); break; }
 
-                await this.publisher.send(['clock', ++this.messageCounter]);
+                await this.publisher.send(['clock', 0]);
+                ++this.messageCounter
                 if (this.messageCounter >= limit) {
+                    t_1 = performance.now();
                     console.log(this.messageCounter, limit);
                     cb();
                     break;
                 }
             }
-            console.log(`Interval worktime: ${(performance.now()-t_0)/1000} seconds`);
+            console.log(`Interval worktime: ${(t_1 - t_0) / 1000} seconds`);
         } catch (e) {
             console.log(e);
         }
+    }*/
+
+    async Run(freq, limit = Number.MAX_SAFE_INTEGER, cb = () => { }) {
+        const targetInterval = 1 / freq * 1000 * 1000; // Целевой интервал в микросекундах
+        console.log(`Целевой интервал: ${targetInterval} мкс`);
+
+        let t0 = performance.now();
+        let t1;
+        const delayMs = targetInterval / 1000;
+        let t_0 = performance.now();
+        let t_1 = t_0 + delayMs;
+        let t_2 = t_1 + delayMs;
+
+        while (this.messageCounter <= limit) {
+            if(this.stopFlag) { t1 = performance.now(); break; }
+
+            await this.publisher.send(['clock', '']);
+            ++this.messageCounter
+            if (this.messageCounter >= limit) {
+                t1 = performance.now();
+                console.log(this.messageCounter, limit);
+                cb();
+                break;
+            }
+            while ((t_1 = performance.now()) - t_0 < delayMs) { }
+            t_0 = t_1;
+            t_1 = t_2;
+            t_2 += delayMs;
+        }
+
+        console.log(`Interval worktime: ${(t1 - t0) / 1000} seconds`);
+        console.log(this.messageCounter, limit);
+        cb();
+
     }
 
     Stop() {
